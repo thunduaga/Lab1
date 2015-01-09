d01d56ea39d6073fb9d5b44522921d351fcfb71e
Chris <Chris@142.232.51.165>
2015-01-08 19:48:14 -0800

    commit 4 students echoing

--- index.php
+++ index.php
@@ -12,11 +12,33 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        //echo "first lab first index";
+        echo "WORK";
         include('student.php');
 
         $students = array();
 
+        //
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+        //
+        
         foreach($students as $student)
         echo $student->toString();
         ?>
